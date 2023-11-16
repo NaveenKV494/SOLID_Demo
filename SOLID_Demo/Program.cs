e532a9a534298735ee16f806ee19e8bd5d964e58
TLSPC-079 <TLSPC-079@DESKTOP-AVJ9472>
2023-11-16 10:44:39 +0530

    added a new class "StandardMessages"

--- SOLID_Demo/Program.cs
+++ SOLID_Demo/Program.cs
@@ -12,18 +12,6 @@ namespace SOLID_Demo
         {
             StandardMessages.WelcomeMessage();
 
-            Person user = PersonDataCapture.Capture();
-
-            bool isUserValid = PersonValidator.Validate(user);
-
-            if (isUserValid == false)
-            {
-                StandardMessages.EndApplication();
-                return;
-            }
-
-            AccountGenerator.CreateAccount(user);
-
             StandardMessages.EndApplication();
         }
     }
